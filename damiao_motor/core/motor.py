dafcac309032e22e3e654341efa0516f9c5f02e8
Philipp Bleimund <philipp.bleimund@hsbi.de>
2026-06-26 11:39:48 +0200

    add helper to retroactively enable can fd

--- damiao_motor/core/motor.py
+++ damiao_motor/core/motor.py
@@ -1048,6 +1048,14 @@ class DaMiaoMotor:
             self._t_max = t_max
 
     # -----------------------
+    # Bus change helpers
+    # -----------------------
+
+    def enable_fd(self, fd: bool = True):
+        """Enables can fd. Baudrate in Register 35 should be greater than 1M."""
+        self._fd = fd
+
+    # -----------------------
     # Setter methods for all writable registers
     # -----------------------
     def set_under_voltage_protection(self, value: float) -> None:
